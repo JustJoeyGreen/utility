71993c5f11fc5537a55ed877b9e783bf6ce83461
jg15253 <joey.green@bristol.ac.uk>
2018-10-23 16:58:28 +0100

    More updates following progression

--- gchqtrail/trail.py
+++ gchqtrail/trail.py
@@ -5,6 +5,9 @@
 import wordlist
 import enchant
 import re
+from pycipher import ColTrans as ct
+from itertools import permutations
+from collections import OrderedDict
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
@@ -77,9 +80,119 @@ def hangman():
 vs y jnf vapbeerpg gura pbhyq bayl or urvtug (ROT13 to avoid spoilers) '''
 
 ################################################################################
+### Back Cover
+################################################################################
+
+def cover_decrypt_bf(debug = True):
+    # Pattern: PGLSS, PMXPY, PACS, PI, PLAMD
+    # Pattern: ABCDD, AEFAG, AHID, AJ, ACHEK
+    english_dict = enchant.Dict('en_GB')
+    wordgen = wordlist.Generator(alphabet)
+    possible_words = list()
+    # First word ABCDD:
+    for poss_1 in wordgen.generate_with_pattern('@@@@@'):
+        # Make sure last two letters the same, and no other letters the same, and it's a word
+        if (poss_1[3] == poss_1[4]) and (len(set(poss_1)) == 4) and english_dict.check(poss_1):
+            # Next word!
+            for poss_2 in wordgen.generate_with_pattern('{}@@{}@'.format(poss_1[0], poss_1[0])):
+                if (len(set(poss_1 + poss_2)) == 7) and english_dict.check(poss_2):
+                    # Next word!
+                    for poss_3 in wordgen.generate_with_pattern('{}@@{}'.format(poss_1[0], poss_1[4])):
+                        if (len(set(poss_1 + poss_2 + poss_3)) == 9) and english_dict.check(poss_3):
+                            # Next word!
+                            for poss_4 in wordgen.generate_with_pattern('{}@'.format(poss_1[0])):
+                                if (len(set(poss_1 + poss_2 + poss_3 + poss_4)) == 10) and english_dict.check(poss_4):
+                                    # Next word!
+                                    for poss_5 in wordgen.generate_with_pattern('{}{}{}{}@'.format(poss_1[0], poss_1[2], poss_3[1], poss_2[1])):
+                                        if (len(set(poss_1 + poss_2 + poss_3 + poss_4 + poss_5)) == 11) and english_dict.check(poss_5):
+                                            possible_words.append((poss_1, poss_2, poss_3, poss_4, poss_5))
+                                            if debug:
+                                                print 'Found: {}'.format((poss_1, poss_2, poss_3, poss_4, poss_5))
+    return possible_words
+
+''' Answer: LRNE AVARGRRA FVKGL GUERR GNXR CHMMYR GENVY GB BOSHFPNGRQ PBQR (ROT13 to avoid spoilers) '''
+''' Ciphetext: AQKLFGHNOPJRISDTUMECBVWXYZ '''
+
+################################################################################
+### Games
+################################################################################
+
+def games_bf(debug = True):
+    # We know: B1, D5, H8, F4
+    # 8 letter word?
+    english_dict = enchant.Dict('en_GB')
+    wordgen = wordlist.Generator(alphabet)
+    possible_words = list()
+    for poss in wordgen.generate_with_pattern('be@fd@@h'):
+        if english_dict.check(poss):
+            possible_words.append(poss)
+    return possible_words
+
+
+################################################################################
+### Transpositional Cipher
+################################################################################
+
+def trans_bf(debug = True):
+    ciphertext = 'RUWDEHMUNHTWNARUGTOETRUNCNASEELRIEEIHMESOACPAEQOIANLENTUMOTSSOOMATRENTHEIHE'
+    phrases = ['TRAIL','THOUSAND','GCHQ','QUESTION','CHRISTMAS','NINETEEN','UNDER']
+
+    for i in range(2, 12):
+        if debug:
+            print "> Trying key length {}".format(i)
+        for key in permutations(alphabet[:i]):
+            mykey = ''.join(key)
+            plaintext = ct(mykey).decipher(ciphertext)
+            if any(word in plaintext for word in phrases):
+                print "+ Key {}: {}".format(mykey, plaintext)
+
+""" Shamelessly nicked from https://stackoverflow.com/questions/28777219/basic-program-to-convert-integer-to-roman-numerals """
+def write_roman(num):
+
+    roman = OrderedDict()
+    roman[1000] = "M"
+    roman[900] = "CM"
+    roman[500] = "D"
+    roman[400] = "CD"
+    roman[100] = "C"
+    roman[90] = "XC"
+    roman[50] = "L"
+    roman[40] = "XL"
+    roman[10] = "X"
+    roman[9] = "IX"
+    roman[5] = "V"
+    roman[4] = "IV"
+    roman[1] = "I"
+
+    def roman_num(num):
+        for r in roman.keys():
+            x, y = divmod(num, r)
+            yield roman[r] * x
+            num -= (r * x)
+            if num > 0:
+                roman_num(num)
+            else:
+                break
+
+    return "".join([a for a in roman_num(num)])
+
+def key_bf(debug = True):
+    ciphertext = 'RUWDEHMUNHTWNARUGTOETRUNCNASEELRIEEIHMESOACPAEQOIANLENTUMOTSSOOMATRENTHEIHE'
+    plaintext = '!!! GET THIS FROM ROT13 OF THE SPOILER !!!'
+    possible_numerals = list()
+    for number in range(1900, 2100):
+        roman = write_roman(number)
+        if ct(roman).decipher(ciphertext) == plaintext:
+            possible_numerals.append((number, roman))
+    return possible_numerals
+
+
+'''Answer: NEBZNAAHZRENYJNFHFRQGBTRARENGRGURPBYHZACREZHGNGVBAURERGURDHRFGVBAVFJUVPUBAR'''
+
+################################################################################
 ### Running
 ################################################################################
 
 if __name__ == "__main__":
 
-    print hangman()
+    print games_bf()
